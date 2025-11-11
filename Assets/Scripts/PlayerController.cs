02a0f925ddc40d3af7aff1b1c5c8026aa5b4ca51
videoJames23 <jjrice429@gmail.com>
2025-11-11 15:07:39 +0000

    Jump
    Player jump added, ground/wall prefab created with separate colliders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,11 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody2D playerRb;
-    private float h;
-    private bool isGrounded;
-    private bool canJump;
+    public bool isGrounded;
     private float speed = 10;
     private float jumpForce = 5;
     
@@ -27,11 +26,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         playerRb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRb.linearVelocity.y);
-       if (Input.GetKeyDown("space"))
+       if (Input.GetKeyDown("space") && isGrounded)
        {
            playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, jumpForce);
        }
         
+    }
+    
+    
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+    
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+            isGrounded = false;
     }
 }
